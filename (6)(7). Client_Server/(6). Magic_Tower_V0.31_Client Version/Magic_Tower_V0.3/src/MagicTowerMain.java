6b400222d1f07fd24640988c283fc6c8375f3967
孙永超 <202283930006@nuist.edu.cn>
2024-12-25 13:11:12 +0800

    Initial commit

--- (6)(7). Client_Server/(6). Magic_Tower_V0.31_Client Version/Magic_Tower_V0.3/src/MagicTowerMain.java
+++ (6)(7). Client_Server/(6). Magic_Tower_V0.31_Client Version/Magic_Tower_V0.3/src/MagicTowerMain.java
@@ -10,11 +10,11 @@ public class MagicTowerMain {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException, InterruptedException {
 		login = new Login();
 		gameData = new GameData();
-		gameData.readMapFromFile("Map.in");
+		gameData.readMapFromFile("Magic_Tower_V0.3/Map.in");
 		gameData.printMap();
 		gui = new GUI(gameData);
 		menu = new Menu(gameData);
-		menu.loadMenu("Menu.XML");
+		menu.loadMenu("Magic_Tower_V0.3/Menu.XML");
 		//menu.displayMenu(menu.rootElement);
 		gameControl = new GameControl(gameData, menu, gui);
 		gameControl.gameStart();
